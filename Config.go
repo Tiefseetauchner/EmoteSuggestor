cd01322300bebb231eeaba0d76fb17a8385187f9
tauchner <7229@htl.rennweg.at>
2021-04-01 01:24:36 +0200

    Added Example Discordgo project

---
+++ Config.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	Token string `json:"Token"`
+}
+
+func LoadConfiguration(file string) Config {
+	var config Config
+	configFile, err := os.Open(file)
+	defer configFile.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	jsonParser := json.NewDecoder(configFile)
+	jsonParser.Decode(&config)
+	return config
+}
+
